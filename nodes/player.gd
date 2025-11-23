83d65bf2e07440fa6be55af0c67ae5e49fe2f634
DDragonYT <91290696+DDragonYT@users.noreply.github.com>
2025-11-23 22:03:20 +1100

    Attempted work on camera

--- nodes/player.gd
+++ nodes/player.gd
@@ -1,10 +1,13 @@
 extends CharacterBody3D
 
-
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
-@export var LOOK_SENSITIVITY = 5
+const LOOK_SENSITIVITY = 0.05
+@onready var camera = $Camera3D
+@onready var animation = $AnimationPlayer
 
+func _ready() -> void:
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -17,24 +20,29 @@ func _physics_process(delta: float) -> void:
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var camera_dir := Input.get_vector("look_up","look_down","look_left","look_right")
+	#var camera_dir := Input.get_vector("look_up","look_down","look_left","look_right")
 	var input_dir := Input.get_vector("move_left", "move_right", "move_forward", "move_back")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	var camera_direction := (transform.basis * Vector3(camera_dir.x, 0, camera_dir.y)).normalized()
-	var camera = $Camera3D
+	#var camera_direction := (transform.basis * Vector3(camera_dir.x, 0, camera_dir.y)).normalized()
 	
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
-		$AnimationPlayer.queue("Step")
+		animation.queue("Step")
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
-		$AnimationPlayer.stop()
+		velocity.x = 0.0
+		velocity.z = 0.0
+		animation.stop()
 	
-	if camera_direction:
-		camera.rotate_x(camera_dir.x/-20)
-		camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-79), deg_to_rad(79))
-		rotate_y(camera_dir.y/-20)
+	#if camera_direction:
+		#camera.rotate_x(camera_dir.x/-20)
+		#camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-79), deg_to_rad(79))
+		#rotate_y(camera_dir.y/-20)
 
 	move_and_slide()
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		rotate_y(-event.relative.x * LOOK_SENSITIVITY)
+		camera.rotate_x(-event.relative.y * LOOK_SENSITIVITY)
+		camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-79), deg_to_rad(79))
